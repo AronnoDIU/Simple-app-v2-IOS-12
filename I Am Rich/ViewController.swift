292d3ca4ae0d369e098e2cb438c87aa724f93450
Yeasir Arafat Aronno <arafatdiu19@gmail.com>
2019-05-16 06:24:05 +0600

    Complete simple app 2

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Yeasir Arafat Aronno on 4/15/19.
+//  Copyright © 2019 Yeasir Arafat Aronno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
